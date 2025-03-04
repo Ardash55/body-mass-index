fc79135dab9c71e00b218031631c35f03e4d35bc
Ardash55 <ardash30042005g@gmail.com>
2025-03-04 21:21:37 +0300

    Добавлен расчет статуса массы тела и его вывод

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function IndexBody() {
   let [massa, setMassa] = useState(0);
   let [hight, setHight] = useState(0);
   let [index, setIndex] = useState(0);
-  let [indexText, setIndexText] = useState('')
+  let [indexText, setIndexText] = useState('');
+  let [massaStatus, setMassaStatus] = useState('');
 
   let handleMassaChange = (e) => setMassa(Number(e.target.value));
   let handleHightChange = (e) => setHight(Number(e.target.value));
+
+  useEffect(() => {
+    if (index == 0) {
+      setMassaStatus(massaStatus = '');
+    } else if (index > 0 && index <= 16) {
+     setMassaStatus(massaStatus = 'Выраженный дефицит массы тела');
+    } else if (index > 16 && index <= 18.5) {
+     setMassaStatus(massaStatus = 'Дефицит массы тела');
+    } else if (index > 18.5 && index <= 25) {
+      setMassaStatus(massaStatus = 'Нормальная масса тела');
+    }  else if (index > 25 && index <= 30) {
+      setMassaStatus(massaStatus = 'Избыточная масса тела (предожирение)');
+    } else if (index > 30 && index <= 35) {
+      setMassaStatus(massaStatus = 'Ожирение первой степени');
+    } else if (index > 35 && index <= 40) {
+      setMassaStatus(massaStatus = 'Ожирение второй степени');
+    } else if (index > 40) {
+      setMassaStatus(massaStatus = 'Ожирение третьей степени');
+    } 
+  }, [index])
 
   return (
     <div>
@@ -56,7 +77,10 @@ function IndexBody() {
       </div>
       <p className='finalOutput'>
         {indexText}
-        </p>
+      </p>
+      <p className='finalOutput'>
+        {massaStatus}
+      </p>
     </div>
   )
 }
